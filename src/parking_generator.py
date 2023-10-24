0f265c33ad21581afc6bee3111de49e18ed2bd82
rohit4242 <rohitluni4242@gmail.com>
2023-10-25 00:34:08 +0530

    init

---
+++ src/parking_generator.py
@@ -0,0 +1,41 @@
+import cv2
+import pickle
+
+
+def generator():
+    width, height = 40, 90
+
+    try:
+        with open("data/source/CarParkPos", "rb") as f:
+            posList = pickle.load(f)
+    except:
+        posList = []
+
+    def mouseClick(events, x, y, flags, params):
+        if events == cv2.EVENT_LBUTTONDOWN:
+            posList.append((x, y))
+        if events == cv2.EVENT_RBUTTONDOWN:
+            for i, pos in enumerate(posList):
+                x1, y1 = pos
+                if x1 < x < x1 + width and y1 < y < y1 + height:
+                    posList.pop(i)
+
+        with open("data/source/CarParkPos", "wb") as f:
+            pickle.dump(posList, f)
+
+    while True:
+        img = cv2.imread("data/source/output.jpg")
+        for pos in posList:
+            cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 2)
+
+        cv2.imshow("Image", img)
+        cv2.setMouseCallback("Image", mouseClick)
+        # exit condition
+        if cv2.waitKey(1) == ord("q"):
+            break
+
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    generator()
